118054a9df53d532369cd64af311298dae87b4fb
Lemonsix <alan.g.asc@gmail.com>
2022-12-20 12:37:12 -0300

    avances en integrador js

--- js_dc/punto_3/js/script.js
+++ js_dc/punto_3/js/script.js
@@ -1,39 +1,18 @@
 let table = [];
-let energyType;
-let description;
-let generatedEnergy;
-let budget;
-let estimatedTime;
-let effectivity;
-let proyecto={
-    energyType,
-    description,
-    generatedEnergy,
-    budget,
-    estimatedTime,
-    effectivity
+document.getElementById("formId").addEventListener("submit", addRow);
+
+function addRow() {
+  alert("hasta aca llega");
+  const proyecto = {
+    energyType: document.getElementById("energia").value,
+    description: document.getElementById("descripcion").value,
+    generatedEnergy: document.getElementById("energiaGenerada").value,
+    budget: document.getElementById("presupuesto").value,
+    estimatedTime: document.getElementById("tiempo").value,
+    //effectivity: (this.generatedEnergy / (this.budget * this.estimatedTime)),
+  };
+  table.push(proyecto);
+  alert(table);
 }
 
-proyecto.energyType="Eolica";
-proyecto.description="asdasdasd";
-proyecto.generatedEnergy=150;
-proyecto.budget=4000;
-proyecto.estimatedTime=6;
-addRow(proyecto);
-
-function addRow(){
-    let newRow=document.createElement("tr");
-    newRow.innerHTML=`<td>${proyecto.energyType}</td>
-    <td>${proyecto.description}</td>
-    <td>${proyecto.generatedEnergy}</td>
-    <td>${proyecto.budget}</td>
-    <td>${proyecto.estimatedTime}</td>
-    <td>${proyecto.effectivity}</td>`;
-    //aca deberia insertarlo en la tabla
-}
-
-
-function effectivityCalculation(generatedEnergy,budget,estimatedTime){
-    return (generatedEnergy/(budget*estimatedTime));
-}
-
+//aca tengo que traer los valores del form
