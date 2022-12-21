21bdb9fc1184401d73fbf74fec0733a0b146a524
Lemonsix <alan.g.asc@gmail.com>
2022-12-21 11:05:25 -0300

    mas avances en proyecto eolico js

--- js_dc/punto_3/js/script.js
+++ js_dc/punto_3/js/script.js
@@ -1,18 +1,33 @@
 let table = [];
-document.getElementById("formId").addEventListener("submit", addRow);
+document.getElementById("myBtn").addEventListener("click", () => addRow()); //llamar por callback
 
 function addRow() {
-  alert("hasta aca llega");
   const proyecto = {
     energyType: document.getElementById("energia").value,
     description: document.getElementById("descripcion").value,
-    generatedEnergy: document.getElementById("energiaGenerada").value,
-    budget: document.getElementById("presupuesto").value,
-    estimatedTime: document.getElementById("tiempo").value,
-    //effectivity: (this.generatedEnergy / (this.budget * this.estimatedTime)),
+    generatedEnergy: Number(document.getElementById("energiaGenerada").value),
+    budget: parseFloat(document.getElementById("presupuesto").value),
+    estimatedTime: Number(document.getElementById("tiempo").value),
   };
+  proyecto.effectivity = proyecto.generatedEnergy / (proyecto.budget * proyecto.estimatedTime);
   table.push(proyecto);
-  alert(table);
+  document.getElementById("tableBody").innerHTML += `
+    <tr>
+      <td>${proyecto.energyType}</td>
+      <td>${proyecto.description}</td>
+      <td>${proyecto.generatedEnergy}</td>
+      <td>${proyecto.budget}</td>
+      <td>${proyecto.estimatedTime}</td>
+      <td>${proyecto.effectivity}</td>
+    </tr>
+    `;
+  let mostEffectiveValue = 0;
+  let mostEffectiveProyect;
+  for (let item of table){
+    if (item.effectivity>mostEffectiveValue){
+      mostEffectiveProyect=item;
+      mostEffectiveValue=item.effectivity;
+    }
+  }
+  //Appendear el proyecto mas efectivo
 }
-
-//aca tengo que traer los valores del form
