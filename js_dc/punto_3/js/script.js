f8b5da2cc22019d3678e7cd18f7a01a25c5e3dd6
Lemonsix <alan.g.asc@gmail.com>
2022-12-22 12:59:21 -0300

    punto 3 finalizado de js

--- js_dc/punto_3/js/script.js
+++ js_dc/punto_3/js/script.js
@@ -2,6 +2,7 @@ let table = [];
 document.getElementById("myBtn").addEventListener("click", () => addRow()); //llamar por callback
 
 function addRow() {
+  //declaracion de objeto proyecto
   const proyecto = {
     energyType: document.getElementById("energia").value,
     description: document.getElementById("descripcion").value,
@@ -11,16 +12,19 @@ function addRow() {
   };
   proyecto.effectivity = proyecto.generatedEnergy / (proyecto.budget * proyecto.estimatedTime);
   table.push(proyecto);
-  document.getElementById("tableBody").innerHTML += `
-    <tr>
-      <td>${proyecto.energyType}</td>
-      <td>${proyecto.description}</td>
-      <td>${proyecto.generatedEnergy}</td>
-      <td>${proyecto.budget}</td>
-      <td>${proyecto.estimatedTime}</td>
-      <td>${proyecto.effectivity}</td>
-    </tr>
-    `;
+
+//agrega el nuevo proyecto al html
+  let htmlTable = document.querySelector('#table'),
+  row = htmlTable.insertRow(1);
+  Object.keys(proyecto).reverse().forEach(key => {
+    row.insertCell(key).innerHTML = proyecto[key];
+  });
+
+//resetea el formulario
+  let formFields = document.getElementById('formId');
+  formFields.reset();
+
+//compara el valor nuevo con el ultimo valor mas efectivo
   let mostEffectiveValue = 0;
   let mostEffectiveProyect;
   for (let item of table){
@@ -29,5 +33,29 @@ function addRow() {
       mostEffectiveValue=item.effectivity;
     }
   }
+
+  //hacer promedio de los 3 tipos de energia
+  let tempArray=table.filter(function(objectProyecto){
+    return objectProyecto.energyType=="Eólica";
+  });
+  let avgTemp = tempArray.reduce((total, next) => total + next.effectivity, 0) / tempArray.length;
+  document.getElementById("eolicaAvg").innerHTML=` ${avgTemp}`;
+
+
+ tempArray=table.filter(function(objectProyecto){
+    return objectProyecto.energyType=="Hidroeléctrica";
+  });
+  avgTemp = tempArray.reduce((total, next) => total + next.effectivity, 0) / tempArray.length;
+  document.getElementById("hidroelectricaAvg").innerHTML=` ${avgTemp}`;
+
+  tempArray=table.filter(function(objectProyecto){
+    return objectProyecto.energyType=="Solar";
+  });
+  avgTemp = tempArray.reduce((total, next) => total + next.effectivity, 0) / tempArray.length;
+  document.getElementById("solarAvg").innerHTML=` ${avgTemp}`;
+
   //Appendear el proyecto mas efectivo
+document.getElementById("effectivitySection").innerHTML=
+`La energía ${mostEffectiveProyect.energyType} es la mas recomendada, con una efectividad de ${mostEffectiveProyect.effectivity}.`
+
 }
